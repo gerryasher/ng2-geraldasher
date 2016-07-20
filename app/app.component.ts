d8d65bb9b28530f4d88e2e1943ca60c0541bfaa6
gerryasher <gerryasher@gmail.com>
2016-07-20 11:45:24 -0700

    Create app.component.ts

---
+++ app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { LinksComponent } from './links/links.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+// additional imports
+
+@Component({
+    selector: 'gasher-app',
+    templateUrl: './app/app.component.html',
+    directives: [ROUTER_DIRECTIVES],
+    providers: []
+})
+export class AppComponent {
+    
+}
