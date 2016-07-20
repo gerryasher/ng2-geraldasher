e19b3749f5eac4b01ec2ede79a893072a5b0a02c
gerryasher <gerryasher@gmail.com>
2016-07-20 11:50:12 -0700

    Delete links.component.ts

--- links/links.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './app/links/links.component.html'
-})
-export class LinksComponent {
-    public pageTitle: string = "Links Page";
-}
