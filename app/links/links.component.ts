99a833aa7fea2faf885e61ee10a5a6e265dab031
gerryasher <gerryasher@gmail.com>
2016-07-20 11:54:12 -0700

    Create links.component.ts

---
+++ app/links/links.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './app/links/links.component.html'
+})
+export class LinksComponent {
+    public pageTitle: string = "Links Page";
+}
