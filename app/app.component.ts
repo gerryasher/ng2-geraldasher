fdde414a8bfac09734cc59fbd03686146a82a8df
gerryasher <gerryasher@gmail.com>
2016-07-20 16:48:11 -0700

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { HTTP_PROVIDERS } from '@angular/http';
+
+import { SkillsService } from './api/skills.service';
 
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -15,8 +18,13 @@ import { ContactComponent } from './contact/contact.component';
     selector: 'gasher-app',
     templateUrl: './app/app.component.html',
     directives: [ROUTER_DIRECTIVES],
-    providers: []
+    providers: [SkillsService, HTTP_PROVIDERS]
 })
 export class AppComponent {
+
+    constructor(private _skillService: SkillsService) {
+        this._skillService.getSkills()
+            .subscribe(posts => console.log(posts));
+    }
     
 }
