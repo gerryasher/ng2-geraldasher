5edf26d63db262895f5b7130cf215186179aa33b
gerryasher <gerryasher@gmail.com>
2016-07-21 15:51:46 -0700

    Update skills.component.ts

--- app/skills/skills.component.ts
+++ app/skills/skills.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from 'angular2/core';
+import { HTTP_PROVIDERS } from 'angular2/http';
+
+import { SkillsService } from '../api/skills.service';
 
 @Component({
-    template: '<h3>{{pageTitle}}</h3>'
+    template: '<h3>{{pageTitle}}</h3>',
+    providers: [SkillsService, HTTP_PROVIDERS]
 })
+
 export class SkillsComponent {
     public pageTitle: string = "Skills Page";
+    
+    constructor (private _skillsService: SkillsService) {
+        this._skillsService.getSkills()
+            .subscribe(skills => console.log(skills));
+    }
 }
