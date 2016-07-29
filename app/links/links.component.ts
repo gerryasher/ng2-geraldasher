fb933272c1d6f7466b5038a39410250d50f358f9
gerryasher <gerryasher@gmail.com>
2016-07-28 17:01:46 -0700

    Update links.component.ts

--- app/links/links.component.ts
+++ app/links/links.component.ts
@@ -5,4 +5,14 @@ import { Component } from 'angular2/core';
 })
 export class LinksComponent {
     public pageTitle: string = "Links Page";
+
+    links: any[] = [
+        {"description": "something", "linkUrl": "http://geraldasher.com"},
+        {"description": "special", "linkUrl": "http://geraldasher.com"},
+        {"description": "Top 10 links", "linkUrl": "http://geraldasher.com"},
+        {"description": "Some top 10", "linkUrl": "http://geraldasher.com"},
+        {"description": "Grwoth", "linkUrl": "http://geraldasher.com"},
+        {"description": "Hacking", "linkUrl": "http://geraldasher.com"}
+    ];
+
 }
