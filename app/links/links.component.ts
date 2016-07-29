05af8097a544bafb18565917d83a68f9c90647d1
gerryasher <gerryasher@gmail.com>
2016-07-29 00:05:29 -0700

    Update links.component.ts

--- app/links/links.component.ts
+++ app/links/links.component.ts
@@ -7,12 +7,12 @@ export class LinksComponent {
     public pageTitle: string = "Links Page";
 
     links: any[] = [
-        {"description": "something", "linkUrl": "http://geraldasher.com"},
-        {"description": "special", "linkUrl": "http://geraldasher.com"},
-        {"description": "Top 10 links", "linkUrl": "http://geraldasher.com"},
-        {"description": "Some top 10", "linkUrl": "http://geraldasher.com"},
-        {"description": "Grwoth", "linkUrl": "http://geraldasher.com"},
-        {"description": "Hacking", "linkUrl": "http://geraldasher.com"}
+        {"description": "Tony Robbins - What Do You Need To Do To Make It Happen?", "linkUrl": "https://www.youtube.com/watch?v=apfTz0Ga6Jo"},
+        {"description": "Tony Robbins's Top 10 Rules For Success", "linkUrl": "https://www.youtube.com/watch?v=O9F19ban7A4"},
+        {"description": "Steve Jobs's Top 10 Rules For Success", "linkUrl": "https://www.youtube.com/watch?v=eHzAtxW3TzY"},
+        {"description": "10 Ways to stop making EXCUSES", "linkUrl": "https://www.youtube.com/watch?v=h8jGoaSx1j4"},
+        {"description": "10 Ways to THINK BIG", "linkUrl": "https://www.youtube.com/watch?v=VMc23pL5WC8"},
+        {"description": "10 ways to be DRIVEN", "linkUrl": "https://www.youtube.com/watch?v=Ok_KzDr-_KU"}
     ];
 
 }
